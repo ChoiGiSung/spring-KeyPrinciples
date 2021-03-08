86bb469300e5a274d13af1d027c974b393ce5e17
gisungchoi <chlrltjd159@naver.com>
2021-03-09 00:54:14 +0900

    핵심원리 이해 2 끝

--- src/main/java/com/example/coco/AppConfig.java
+++ src/main/java/com/example/coco/AppConfig.java
@@ -1,19 +1,45 @@
 package com.example.coco;
 
+import com.example.coco.discount.DisCountPolicy;
 import com.example.coco.discount.FixDiscountPolicy;
+import com.example.coco.discount.RateDiscountPolicy;
+import com.example.coco.member.MemberRepository;
 import com.example.coco.member.MemberService;
 import com.example.coco.member.MemberServiceImpl;
 import com.example.coco.member.MemoryMemberRepository;
 import com.example.coco.order.OrderService;
 import com.example.coco.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class AppConfig {
+    //장점
+    //역할과 구현 클래스가 한 눈에 보인다. - > 애플리케이션 전체 구성이 보인다
 
+    //변경 사항이 있으면 구성영역만 변경됨
+    //사용 영역은 변경 x
+
+    @Bean
     public MemberService memberService() {
-        return new MemberServiceImpl(new MemoryMemberRepository());
+        return new MemberServiceImpl(memberRepository());
     }
 
+    @Bean
     public OrderService orderService() {
-        return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+        return new OrderServiceImpl(memberRepository(), disCountPolicy());
     }
+
+    @Bean
+    public MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
+    }
+
+    @Bean
+    public DisCountPolicy disCountPolicy() {
+//        return new FixDiscountPolicy();
+        // 할인 정책 변경
+        return new RateDiscountPolicy();
+    }
+
 }
--- src/main/java/com/example/coco/MemberApp.java
+++ src/main/java/com/example/coco/MemberApp.java
@@ -4,12 +4,23 @@ import com.example.coco.member.Grade;
 import com.example.coco.member.Member;
 import com.example.coco.member.MemberService;
 import com.example.coco.member.MemberServiceImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class MemberApp {
     public static void main(String[] args) {
 //        MemberService memberService=new MemberServiceImpl();
-        AppConfig appConfig=new AppConfig();
-        MemberService memberService = appConfig.memberService();
+//        AppConfig appConfig=new AppConfig();
+//        MemberService memberService = appConfig.memberService();
+
+
+        //스프링 컨테이너
+        ApplicationContext applicationContext=new AnnotationConfigApplicationContext(AppConfig.class);
+
+        //빈 꺼내기기
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
+
+
 
         Member member = new Member(1L, "memberA", Grade.BASIC);
         memberService.join(member);
